26c0829a2365c4e9f668d22d5f3d417c515d6cce
Femi Oladele <profemzy@gmail.com>
2020-01-06 19:51:57 +0100

    Update App Welcome Message

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World\n');
+    res.send('Hello Jenkins\n');
 });
 app.listen(PORT, HOST);
 console.log( `Running on http://${HOST}:${PORT}`);
