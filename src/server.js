f3f028c7e1cfb3761517a4b6ea63a64ac5f64738
Femi Oladele <profemzy@gmail.com>
2020-01-06 20:04:00 +0100

    Update Default Page

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello Jenkins\n');
+    res.send('Hello Jenkins NodeJS Kubernetes\n');
 });
 app.listen(PORT, HOST);
 console.log( `Running on http://${HOST}:${PORT}`);
